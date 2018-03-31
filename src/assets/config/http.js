1df43884f62cb27976bbc4da64b0410cc2147460
Aertes <mrxu1010@outlook.com>
2018-03-31 10:21:26 +0800

    login_bg

---
+++ src/assets/config/http.js
@@ -0,0 +1,39 @@
+import Axios from 'axios'
+Axios.defaults.withCredentials=true;
+
+//get请求
+function get(url) {
+  let getUrl = new RegExp('http').test(url) ? url : `${BASE_URL}${url}`;
+  return Axios({
+    url: getUrl
+  })
+}
+
+//post请求
+function post(url, data) {
+  let getUrl = new RegExp('http').test(url) ? url : `${BASE_URL}${url}`;
+  return Axios({
+    method: 'post',
+    url: getUrl,
+    data: data
+  })
+}
+
+//上传表单请求
+function uploadPost(url, data) {
+  let getUrl = new RegExp('http').test(url) ? url : `${BASE_URL}${url}`;
+  return Axios({
+    method: 'post',
+    url: getUrl,
+    data: data,
+    headers: {
+      'enctype': 'multipart/form-data'
+    }
+  })
+}
+
+export {
+  get,
+  post,
+  uploadPost
+}
--- src/assets/config/xhrUrls.js
+++ src/assets/config/xhrUrls.js
@@ -1,5 +1,25 @@
 export default {
   CODE: '/auto/code',
   LOGIN: '/auto/login',
-  CMAUPLOAD: '/campaign/upload'
+
+  CMA_UPLOAD: '/campaign/upload',
+  CMA_SEARCH:'/campaign/search',
+
+  COM_UPLOAD: '/com/upload',
+  COM_SEARCH:'/com/search',
+
+  CRM_UPLOAD: '/crm/upload',
+  CRM_SEARCH:'/crm/search',
+
+  EC_UPLOAD: '/ec/upload',
+  EC_SEARCH:'/ec/search',
+
+  RV_UPLOAD: '/review/upload',
+  RV_SEARCH:'/review/search',
+
+  OV_CMA_UPLOAD: '/overview/campaign/search',
+  OV_COM_SEARCH:'/overview/com/search',
+  OV_CRM_SEARCH:'/overview/crm/search',
+  OV_RV_SEARCH:'/overview/review/search',
+
 }
